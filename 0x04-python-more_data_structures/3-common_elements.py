701b7046ce91c82149fd12d6d5011757b087df50
Mallaimy <bokormallaimy@gmail.com>
2022-06-09 11:33:48 +0100

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    list(set_1)
-    list(set_2)
-    set_3 = []
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                set_3.append(i)
-                print(set_3)
+    return set_1 & set_2
